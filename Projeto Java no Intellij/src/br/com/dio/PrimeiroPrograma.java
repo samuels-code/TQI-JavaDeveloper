bae5049b0dc640103bd59c38d2f1ea6aaae6387e
Samuel Silva <93296784+samuels-code@users.noreply.github.com>
2021-11-29 08:34:56 -0300

    Projeto Simples usando Intellij

---
+++ Projeto Java no Intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+import model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("O resultado é: " + (a+b));*/
+    }
+}
+
